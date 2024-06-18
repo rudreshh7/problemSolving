2b073ed5a4a447b639426f7de2112094e6b9260f
Rudresh Jha <kumarrudresh5@gmail.com>
2024-06-18 21:36:00 +0530

    solved few pattern questions

---
+++ basic/patterns/patternFour.java
@@ -0,0 +1,5 @@
+public class patternFour{
+	public static void main(String[] args) {
+		System.out.println("Hello nigga");
+	}
+}
---
+++ basic/patterns/patternTwo.java
@@ -0,0 +1,16 @@
+public class patternTwo{
+	public static void main(String[] args) {
+		System.out.println("Implementation of Triangle Pattern");
+		for (int i =0;i<=5 ;i++ ) {
+			for (int j =0;j<=i ;j++ ) {
+				System.out.print("*");
+
+				
+			}
+			System.out.println();
+			
+		}
+
+		
+	}
+}
